a2d98c56c771ff971bae115e3a4811f85401e3fd
ahmad-v0 <ahmad.prov0@gmail.com>
2025-05-25 23:46:21 +0600

    base path on vite.config.js repaired

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: 'https://ahmad-v0.github.io/sc-pr-react-basic/'
+  base: '/sc-pr-react-basic/'
 })
